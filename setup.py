4f9477f4ce069b87ce82a3e80f0c87d11ad4a774
Aleksey Porfirov <lexqt@yandex.ru>
2016-09-20 11:58:50 +0300

    [0.4.1.2] Fix packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='django-chunks',
-      version='0.4.1.1',
+      version='0.4.1.2',
       description='Keyed blocks of content for use in your Django templates',
       author='Clint Ecker',
       author_email='me@clintecker.com',
       url='http://code.google.com/p/django-chunks/',
-      packages=['chunks', 'chunks.templatetags'],
+      packages=find_packages(),
+      include_package_data=True,
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
